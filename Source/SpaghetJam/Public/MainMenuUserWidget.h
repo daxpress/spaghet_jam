ed2f30902d7ae56879bc45555b370bb1257537e8
Andrea Voscilla <shinyvosci@gmail.com>
2025-04-11 09:56:53 +0200

    Source

---
+++ Source/SpaghetJam/Public/MainMenuUserWidget.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainMenuUserWidget.generated.h"
+UCLASS()
+class SPAGHETJAM_API UMainMenuUserWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+
+	public:
+	
+	UFUNCTION(BlueprintCallable)
+	void LoadLevelAsync();
+};
